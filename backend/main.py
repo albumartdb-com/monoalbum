2e6776aa06d4053c905ea6ebc8b6014a4b2ab958
Trent Wilson <trent234@gmail.com>
2021-04-27 19:15:52 -0700

    connect to db

--- backend/main.py
+++ backend/main.py
@@ -1,18 +1,21 @@
 # demonstration level python code
 # goal: get a spotify album cover and store it in mongodb
 
+import os
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from dotenv import load_dotenv
 from pymongo import MongoClient
+import json
 
 # load envs from .env for spotify creds
 load_dotenv()
 
 # connect to mongo db
-mongo_client = MongoClient('localhost', 27017)
-db = mongo_client.albumart_db
-collection = db.art
+mongo_client = MongoClient(username=os.getenv('MONGO_ROOT_USER'), 
+        password=os.getenv('MONGO_ROOT_PASSWORD'), host='localhost', port=27017)
+mongo_db = mongo_client.albumart_db
+mongo_collection = mongo_db.art
 
 # get access token for spotify
 spotify = spotipy.Spotify(
@@ -28,3 +31,12 @@ results = spotify.artist_top_tracks(lz_uri)
 # results
 for track in results['tracks'][:10]:
     print(track['album']['images'][0]['url'])
+    document = {
+            "name": track['album']['name'],
+            "albumart_url": track['album']['images'][0]['url']
+            }
+    document_id = mongo_collection.insert_one(document).inserted_id
+
+#for track in results['tracks']:
+#    print(track)
+    #print(json.dumps(track))
