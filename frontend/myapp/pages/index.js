d2ee1acf954c4a92babc4d50472c42e0c5a78d6f
Kyle Gustke <kaigustke@gmail.com>
2021-06-05 14:56:06 -0700

    Search will fetch data from spotify using the search value

--- frontend/myapp/pages/index.js
+++ frontend/myapp/pages/index.js
@@ -1,18 +1,14 @@
 import Head from 'next/head'
-import {useState, useEffect} from 'react'
 import SearchBar from '../components/searchbar'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  const [input, setInput] = useState('')
-
   return (
     <div className={styles.container}>
       <Head>
         <title>monoalbum</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
 
       <main className={styles.main}>
         <SearchBar
