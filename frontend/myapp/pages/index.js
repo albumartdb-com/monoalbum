ffbe3b6d1809935ceb4c30567e7128447450ddc1
Orion Crocker <orioncrocker@gmail.com>
2021-06-05 16:03:34 -0700

    updated index.js

--- frontend/myapp/pages/index.js
+++ frontend/myapp/pages/index.js
@@ -1,9 +1,6 @@
 import Head from 'next/head'
-<<<<<<< HEAD
 import {useState, useEffect} from 'react'
 import NavBar from '../components/navbar'
-=======
->>>>>>> d2ee1acf954c4a92babc4d50472c42e0c5a78d6f
 import SearchBar from '../components/searchbar'
 import styles from '../styles/Home.module.css'
 
@@ -15,11 +12,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-<<<<<<< HEAD
       <NavBar/>
 
-=======
->>>>>>> d2ee1acf954c4a92babc4d50472c42e0c5a78d6f
       <main className={styles.main}>
         <SearchBar
         />
