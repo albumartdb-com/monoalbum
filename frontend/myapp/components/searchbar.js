41680831c1fbe79c2161c07d9664440e882d62ff
Orion Crocker <orioncrocker@gmail.com>
2021-06-05 15:42:40 -0700

    merged kyle's changes with latest commit

--- frontend/myapp/components/searchbar.js
+++ frontend/myapp/components/searchbar.js
@@ -1,12 +1,30 @@
+import {useState} from 'react'
+
 export default function SearchBar() {
+    const [input, setInput] = useState('')
+    
+    function handleSubmit(e) {
+        e.preventDefault()
+        fetch(`http://localhost:5000/?q=${input}`, {
+            method: "GET" 
+        })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch((error) => {
+            console.error('Error:', error)
+        });
+    }
+
     return (
-        <form action="/" method="get">
+        <form method="get" onSubmit={e => handleSubmit(e)}>
             <label htmlFor="header-search">
                 <span className="visually-hidden">Search monoalbum</span>
             </label>
             <input
                 type="text"
                 placeholder="Search for albums, artists, singles"
+                onChange={e => setInput(e.target.value)}
+                value={input}
                 name="s"
             />
             <button type="submit">Search</button>
