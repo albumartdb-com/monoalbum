ac22aba76289b92c44b1de8755fbed5094cbf43c
Kai <kaigustke@gmail.com>
2021-06-09 13:31:46 -0700

    Displays albums and artists on search

--- frontend/myapp/components/searchbar.js
+++ frontend/myapp/components/searchbar.js
@@ -1,33 +1,23 @@
 import {useState} from 'react'
 
-export default function SearchBar() {
+export default function SearchBar({onSubmit}) {
     const [input, setInput] = useState('')
-    
-    function handleSubmit(e) {
-        e.preventDefault()
-        fetch(`http://localhost:5000/?q=${input}`, {
-            method: "GET" 
-        })
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch((error) => {
-            console.error('Error:', error)
-        });
-    }
 
     return (
-        <form method="get" onSubmit={e => handleSubmit(e)}>
-            <label htmlFor="header-search">
-                <span className="visually-hidden">Search monoalbum</span>
-            </label>
-            <input
-                type="text"
-                placeholder="Search for albums, artists, singles"
-                onChange={e => setInput(e.target.value)}
-                value={input}
-                name="s"
-            />
-            <button type="submit">Search</button>
-        </form>
+        <>
+            <form method="get" onSubmit={e => onSubmit(e, {input})}>
+                <label htmlFor="header-search">
+                    <span className="visually-hidden">Search monoalbum</span>
+                </label>
+                <input
+                    type="text"
+                    placeholder="Search for albums, artists, singles"
+                    onChange={e => setInput(e.target.value)}
+                    value={input}
+                    name="s"
+                />
+                <button type="submit">Search</button>
+            </form>
+        </>
     )
 }
--- frontend/myapp/pages/index.js
+++ frontend/myapp/pages/index.js
@@ -5,6 +5,24 @@ import SearchBar from '../components/searchbar'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const [albums, setAlbums] = useState([])
+  const [artists, setArtists] = useState([])
+
+  function handleSubmit(event, input) {
+      event.preventDefault()
+      fetch(`http://localhost:5000/?q=${input}`, {
+          method: "GET" 
+      })
+      .then(response => response.json())
+      .then(data => {
+        setAlbums(data.albums.items)
+        setArtists(data.artists.items)
+      })
+      .catch((error) => {
+          console.error('Error:', error)
+      });
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -16,7 +34,22 @@ export default function Home() {
 
       <main className={styles.main}>
         <SearchBar
+          onSubmit={handleSubmit}
         />
+        <ul>
+          {
+            Object.entries(albums).slice(0, 10).map(([key, value]) => {
+              return <li id={value.id}>{value.name}</li>
+            })
+          }
+        </ul>
+        <ul>
+          {
+            Object.entries(artists).slice(0, 10).map(([key, value]) => {
+              return <li id={value.id}>{value.name}</li>
+            })
+          }
+        </ul>
       </main>
     </div>
   )
